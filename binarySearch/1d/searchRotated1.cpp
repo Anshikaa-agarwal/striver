ab1085fb5748b46e52d1b3c2cc151df39e70369a
Anshika <anshika2020ag@gmail.com>
2025-01-09 11:24:13 +0530

    search in rotated array 1

--- binarySearch/1d/searchRotated1.cpp
+++ binarySearch/1d/searchRotated1.cpp
@@ -11,17 +11,44 @@ int searchRotated1(vector<int> arr, int target) {
     int low=0, high=arr.size()-1, mid;
 
     while(low<=high) {
-        mid = low+(high-low)/2;
-        if(arr[low] <= arr[mid]) {
-            // left half is sorted
+        int mid = (low+high)/2;
+        if(arr[mid]==target) return mid;
+        
+        // find which half is sorted
+        if(arr[low]<=arr[mid]) {
+            //left half is sorted
 
-        } else if(arr[low]>arr[mid]) {
-            // right array is sorted
-            
+            // now check if target lies b/w arr[low] and arr[mid]
+            // if it lies, perform BS on it, otherwise eliminate it
+
+            if(arr[low] <= target && target <= arr[mid]) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        } 
+        else if(arr[mid]<=arr[high]) {
+            // right half is sorted
+
+            // check if target element lies b/w arr[mid] and arr[high]
+            // if yes, apply bs here, otherwise eliminate it
+
+            if(arr[mid]<=target && target<=arr[high]) {
+                low = mid+1;
+            } else {
+                high = mid - 1;
+            }
         }
     }
+    return -1;
 }
 int main() {
     vector<int> arr = {7,8,9,1,2,3,4,5,6};
+    int target = 1;
 
+    int search = searchRotated1(arr, target);
+    if(search==-1) cout << "target not found.";
+    else cout << "Target " << target << " found at index " << search;
+
+    return 0;
 }
