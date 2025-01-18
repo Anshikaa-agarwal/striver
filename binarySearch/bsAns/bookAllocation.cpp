77156da6d8fcd5236235249154a86839bdcd7866
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-01-18 14:40:31 +0530

    book allocation

--- binarySearch/bsAns/bookAllocation.cpp
+++ binarySearch/bsAns/bookAllocation.cpp
@@ -13,3 +13,54 @@ If the allocation of books is not possible. return -1
 #include<vector>
 #include<algorithm>
 using namespace std;
+int sumArr(vector<int> arr) {
+    int sum=0;
+    for(int i=0; i<arr.size(); i++) sum+=arr[i];
+    return sum;
+}
+int numOfStudents(vector<int>& books, int minPages) {
+    // this func returns for given num of min pages, how many students would be there
+    int student=1, currentPages=0; // currently we're at student 1 and she has 0 pages
+
+    for(int i=0; i<books.size(); i++) {
+        if(currentPages+books[i]<=minPages) {
+            currentPages += books[i]; // if student can hold book[i], add pages to currentPages
+        } else{
+            student++;
+            currentPages=books[i]; // otherwise move to next student and reset currentPages to 0.
+        }
+    }
+    return student;
+}
+int minOfMaxPages(vector<int> &books, int students) {
+    // takes input of books array and number of students the book must be divided to that each have atleast a book
+    if(books.size()<students) return -1;
+    if(books.size()==students) return *max_element(books.begin(), books.end());
+
+    int low=*max_element(books.begin(), books.end()); //lowest amt of page a student can have
+    int high=sumArr(books); //highest being she has all books
+    int ans=-1, mid;
+
+    //if for x min num of pages we have more than req students, we would need to inc min num of pages
+    //if for x min num of pages we have exactly the num of req students, we'll look for a greater num to see if conditions are satisfied
+    //if for x min num of pages we have less than req students, we need to dec req num of students
+
+    while(low<=high) {
+        mid = low+(high-low)/2;
+        int s = numOfStudents(books, mid);
+        if(s <= students) {  // Check for <= students instead of == 
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int main() {
+    vector<int> books = {12, 34, 67, 90};
+    int n = 4;
+    cout << minOfMaxPages(books, n);
+
+    return 0;
+}
