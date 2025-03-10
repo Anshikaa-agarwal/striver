eb0889bb6a44b02823bc941ce28fde06aa280335
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-03-10 18:45:07 +0530

    insert at end, middle

--- ll/intro.cpp
+++ ll/intro.cpp
@@ -24,6 +24,37 @@ void insertHead(node* &head, int value) {
     temp->next = head;
     head = temp;
 }
+
+void insertEnd(node* &head, int value) {
+    node* temp = head;
+    node* newNode = new node(value);
+    //move to end of linkedlist
+    while(temp->next!=nullptr) {
+        temp = temp -> next;
+    }
+    temp -> next = newNode;
+}
+
+void insertIndex(node* &head, int index, int value) {
+    node* temp = head;
+    node* newNode = new node(value);
+
+    for(int i=0; i<index-1; i++) {
+        if(temp==nullptr) {
+            cout << "index out of bounds.";
+            return;
+        }
+        temp=temp->next;
+    }
+
+    if (temp == nullptr) {
+        cout << "Index out of bounds" << endl;
+        return;
+    }
+
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
 void printLL(node* &head) {
     node* temp = head;
     while(temp!=nullptr) {
@@ -36,6 +67,9 @@ int main() {
     insertHead(head, 5);
     insertHead(head, 4);
     insertHead(head, 3);
+    insertEnd(head, 13);
+
+    insertIndex(head, 3, 6);
     printLL(head);
 
     return 0;
