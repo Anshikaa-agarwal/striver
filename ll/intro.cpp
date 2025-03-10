66c96a98a8856ecb09d8d67cbece14c11d4e4328
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-03-10 18:25:34 +0530

    insert at head

--- ll/intro.cpp
+++ ll/intro.cpp
@@ -19,7 +19,24 @@ class node {
         this->next = nullptr;
     }
 };
+void insertHead(node* &head, int value) {
+    node* temp = new node(value);
+    temp->next = head;
+    head = temp;
+}
+void printLL(node* &head) {
+    node* temp = head;
+    while(temp!=nullptr) {
+        cout << temp->data << " ";
+        temp = temp ->next;
+    }
+}
 int main() {
-    node* newNode = new node(12);
-    cout << newNode->data;
+    node* head = new node(12);
+    insertHead(head, 5);
+    insertHead(head, 4);
+    insertHead(head, 3);
+    printLL(head);
+
+    return 0;
 }
