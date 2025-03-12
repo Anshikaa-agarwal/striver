76c6d727006445fb052f1bae4491f33b4e2070de
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-03-12 22:15:18 +0530

    intro - doubly linked list

--- ll/double/intro.cpp
+++ ll/double/intro.cpp
@@ -24,5 +24,28 @@ class node{
 
 int main() {
     vector<int> arr = {1,2,3,4,5};
+    node* head = new node(arr[0]);
+    node* current = head;
 
+    for(int i=1; i<arr.size(); i++) {
+        node* temp = new node(arr[i]);
+        current->next=temp;
+        temp->prev=current;
+
+        current=temp;
+    }
+
+    node* ptr = head;
+    cout << "Forward: ";
+    while(ptr != nullptr) {
+        cout << ptr->data << " ";
+        ptr = ptr->next;
+    }
+
+    cout << "\nBackward: ";
+    ptr = current; // current is now the last node
+    while(ptr != nullptr) {
+        cout << ptr->data << " ";
+        ptr = ptr->prev;
+    }
 }
