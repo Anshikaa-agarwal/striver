fd5598311cc31ffeda41519c358b26479fc8f395
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-03-12 22:33:04 +0530

    insertion at head - dll

---
+++ ll/double/insertion.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* prev;
+    node* next;
+
+    public:
+    node(int value, node* prev1, node* next1) {
+        this->data=value;
+        this->prev=prev1;
+        this->next=next1;
+    }
+
+    node(int value) {
+        this->data=value;
+        this->prev=nullptr;
+        this->next=nullptr;
+    }
+};
+
+void insertHead(node* &head, int value) {
+    node* temp = new node(value);
+    temp->next=head;
+    head->prev=temp;
+    head=temp;
+}
+
+void printLL(node* head) {
+    if(head==nullptr) return;
+    node* temp = head;
+    while(temp!=nullptr) {
+        cout << temp->data << " ";
+        temp=temp->next;
+    }
+}
+
+int main() {
+    node* head = new node(5);
+    insertHead(head, 4);
+    insertHead(head, 3);
+    insertHead(head, 2);
+    insertHead(head, 1);
+
+    printLL(head);
+    return 0;
+}
