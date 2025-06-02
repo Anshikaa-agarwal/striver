178c48e78d77039addbd5d516c7448dd313ce4f3
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-06-02 20:24:31 +0530

    moore's voting algo - n/3

--- Array/hard/majority2.cpp
+++ Array/hard/majority2.cpp
@@ -37,7 +37,7 @@ vector<int> majority(vector<int> nums) {
     return result;
 }
 
-vector<int> majorit2(vector<int> &nums) {
+vector<int> majority2(vector<int> &nums) {
     // use hashmap
     // Better - o(n) avg, o(n^2) worst case but very rare
     unordered_map<int, int> mpp;
@@ -57,6 +57,71 @@ vector<int> majorit2(vector<int> &nums) {
 
     return result;
 }
+
+vector<int> majority3(vector<int> &nums) {
+
+    sort(nums.begin(), nums.end());
+
+    vector<int> result;
+    int n = nums.size();
+    int count = 1;
+
+    for(int i=1; i<nums.size(); i++) {
+        if(nums[i] == nums[i-1]) {
+            count++;
+        }
+        else {
+            if(count > n/3) {
+                result.push_back(nums[i-1]);
+            }
+            count = 1;
+        }
+
+        if(result.size() == 2)
+        break;
+    }
+
+    if(count > n/3) {
+        result.push_back(nums[n-1]);
+    }
+
+    return result;
+}
+
+vector<int> optimal(vector<int> &nums) {
+    int c1 = 0, c2 = 0;
+    int el1 = INT32_MIN, el2 = INT32_MIN;
+    int n = nums.size();
+
+    for(int i=0; i<n; i++) {
+        if(c1 == 0 && nums[i] != el2) {
+            c1++;
+            el1 = nums[i];
+        }
+        else if(c2 == 0 && nums[i] != el1) {
+            c2++;
+            el2 = nums[i];
+        }
+        else if(nums[i] == el1) c1++;
+        else if(nums[i] == el2) c2++;
+        else {
+            c1--;
+            c2--;
+        }
+    }
+
+    vector<int> result;
+    c1=0, c2=0;
+    for(int i=0; i<n; i++) {
+        if(nums[i] == el1) c1++;
+        else if(nums[i] == el2) c2++;
+    }
+
+    if(c1 > n/3) result.push_back(el1);
+    if(c2 > n/3) result.push_back(el2);
+
+    return result;
+}
 void printArr(vector<int> &nums) {
     for(int i=0; i<nums.size(); i++) 
     cout << nums[i] << " ";
@@ -65,12 +130,20 @@ void printArr(vector<int> &nums) {
 int main() {
     vector<int> nums = {11,33,33,11,33,11};
     vector<int> result = majority(nums);
-    vector<int> result2 = majorit2(nums);
+    vector<int> result2 = majority2(nums);
+    vector<int> result3 = majority3(nums);
+    vector<int> result4 = optimal(nums);
 
     printArr(result);
     cout << endl;
 
     printArr(result2);
+    cout << endl;
+
+    printArr(result3);
+    cout << endl;
+
+    printArr(result4);
     cout << endl;
 
     return 0;
