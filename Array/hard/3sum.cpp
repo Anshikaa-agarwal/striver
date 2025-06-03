669ef47ef68593118c20e2de30b6a1b94a29695e
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-06-03 20:22:35 +0530

    3sum - brute o(n^2 log k)

--- Array/hard/3sum.cpp
+++ Array/hard/3sum.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int> &nums) {
+    // O(n^3 * log(no. of unique triplets))
     set<vector<int>> st;
 
     for(int i=0; i<nums.size(); i++) {
@@ -24,6 +25,32 @@ vector<vector<int>> threeSum(vector<int> &nums) {
     return result;
 }
 
+vector<vector<int>> threeSum2(vector<int> &nums) {
+    // hashing
+    // O(n^2 log k)
+    unordered_map<int, int> mpp;
+    set<vector<int>> st;
+
+    for(int i=0; i<nums.size(); i++) {
+        mpp[nums[i]] = i;
+    }
+
+    for(int i=0; i<nums.size(); i++) {
+        for(int j=i+1; j<nums.size(); j++) {
+            int third = -(nums[i] + nums[j]);
+            auto it = mpp.find(third);
+            if(it != mpp.end() && it->second != j && it->second != i) {
+                vector<int> temp = {nums[i], nums[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
+            } 
+        }
+    }
+
+    vector<vector<int>> result(st.begin(), st.end());
+    return result;
+}
+
 void printArr(vector<int> arr) {
     for(int i=0; i<arr.size(); i++) 
     cout << arr[i] << " ";
@@ -31,7 +58,7 @@ void printArr(vector<int> arr) {
 
 int main() {
     vector<int> arr = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> result = threeSum(arr);
+    vector<vector<int>> result = threeSum2(arr);
 
     for(int i=0; i<result.size(); i++) {
         printArr(result[i]);
